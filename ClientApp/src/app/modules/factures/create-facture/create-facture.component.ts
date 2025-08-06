b58d1f4fe14336e3259e44f243e9b20550ea7c64
safiylm <121391404+safiylm@users.noreply.github.com>
2025-08-06 17:06:47 +0200

    create facture

--- ClientApp/src/app/modules/factures/create-facture/create-facture.component.ts
+++ ClientApp/src/app/modules/factures/create-facture/create-facture.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FactureService } from '../../../core/facture-service';
+import { ProduitService } from '../../../core/produit-service';
 
 @Component({
   selector: 'app-create-facture',
@@ -7,13 +9,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateFactureComponent implements OnInit {
 
-  constructor() { }
-  clientchoix ="create"
-  clientId = ""
+  constructor(
+    private factureService: FactureService, private produitService: ProduitService) { }
+  listeProduits !: any;
+  resultat=""
+  clientchoix ="select"
+  clientId = 0
+  formProduitIsSubmit = false;
+
   ngOnInit(): void {
   }
 
-  addItem(newItem: string) {
+  getClientId(newItem: number) {
     this.clientId = newItem
   }
+
+  facture = {
+    clientId: this.clientId,
+    userId: 1,
+    titre: 'Facture ',
+    totalTVA: 0,
+    totalHT: 0,
+    informations: "",
+    createdAt: new Date()
+
+  };
+
+  getProduits(listeproduits: any) {
+    this.formProduitIsSubmit = true;
+    this.listeProduits = listeproduits[0]
+    this.facture.clientId = this.clientId;
+    this.facture.totalHT = listeproduits[1]
+    this.facture.totalTVA = listeproduits[2]
+  }
+
+
+  save() {
+    this.factureService.create(this.facture)
+      .subscribe(
+        (data) => {
+           let  jsonObj = JSON.parse(data); // string to "any" object first
+          console.log(jsonObj["id"])
+          for (let prod of this.listeProduits)
+          prod.factureId = jsonObj["id"];
+
+          this.produitService.create(this.listeProduits).subscribe(
+              (data1) => {
+              console.log(data1)
+              this.resultat= data1
+              })
+          
+        }
+      )
+   
+  }
+
+  openCreateForm() {
+    this.clientchoix = "create"
+  }
+
+  openSelectForm() {
+    this.clientchoix = "select"
+  }
+
 }
