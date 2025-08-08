46f9f83593c5809e037ac22980ce40e4257a88e7
safiylm <121391404+safiylm@users.noreply.github.com>
2025-08-08 22:36:38 +0200

    add new informations like nb clients & total montant du mois

--- ClientApp/src/app/modules/factures/liste-factures/liste-factures.component.ts
+++ ClientApp/src/app/modules/factures/liste-factures/liste-factures.component.ts
@@ -13,16 +13,36 @@ export class ListeFacturesComponent implements OnInit {
 
   liste !: Facture[];
   resultat = "";
-  
+  searchFacture = ""
+  totalFacture = 0
+  factureEnAttente = 0
   constructor(private factureService: FactureService) { }
 
   ngOnInit(): void {
 
-    this.factureService.getFactures().subscribe((fac) => {
+    this.factureService.getFacturesByAuteurId(Number(localStorage.getItem("userId"))).subscribe((fac) => {
       this.liste = fac;
+      for (let f of this.liste) {
+
+        if (f.status == "Payés")
+          this.totalFacture = this.totalFacture + f.totalHT + f.totalTVA
+
+        if (f.status.trim() == "En Attente")
+          this.factureEnAttente = this.factureEnAttente + 1
+      }
+      localStorage.setItem("factureEnAttente", this.factureEnAttente.toString())
+      localStorage.setItem("totalFacture", this.totalFacture.toString())
+
+
     });
+  }
 
+  search() {
+    for (let fact of this.liste) {
+      if (this.searchFacture == fact.titre) {
 
+      }
+    }
   }
 
 
--- ClientApp/src/app/modules/home/home.component.ts
+++ ClientApp/src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,16 @@ import { Component } from '@angular/core';
 
 })
 export class HomeComponent {
+ nbClients = localStorage.getItem("nbClients")
+ factureEnAttente = localStorage.getItem("factureEnAttente")
+  total = localStorage.getItem("totalFacture")
 
+  constructor( private route: Router) { }
+  logout() {
+    localStorage.setItem("isLoggedIn", "false")
+    localStorage.setItem('userId', '');
+    localStorage.removeItem('userId')
+    localStorage.removeItem('token');
+    this.route.navigate(['/login']);
+  }
 }
