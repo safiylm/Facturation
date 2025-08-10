91d1cd9322ac1fddd99e65207a6d6bfa3dd4a20c
safiylm <121391404+safiylm@users.noreply.github.com>
2025-08-10 20:52:17 +0200

    Update app.module.ts

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
@@ -8,45 +8,25 @@ import { AppComponent } from './app.component';
 import { NavMenuComponent } from './layout/nav-menu/nav-menu.component';
 import { HomeComponent } from './modules/home/home.component';
 import { CounterComponent } from './modules/counter/counter.component';
-import { FetchDataComponent } from './modules/fetch-data/fetch-data.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
 import { CommonModule } from '@angular/common';
-import { CreateClientComponent } from './modules/clients/create-client/create-client.component';
-import { EditClientComponent } from './modules/clients/edit-client/edit-client.component';
-import { EditUserComponent } from './modules/user/edit-user/edit-user.component';
 import { ListeFacturesComponent } from './modules/factures/liste-factures/liste-factures.component';
-import { CreateFactureComponent } from './modules/factures/create-facture/create-facture.component';
 import { ListeClientsComponent } from './modules/clients/liste-clients/liste-clients.component';
-import { SelectClientComponent } from './modules/clients/select-client/select-client.component';
-import { DetailClientComponent } from './modules/clients/detail-client/detail-client.component';
-import { EditProduitComponent } from './modules/produits/edit-produit/edit-produit.component';
-import { CreateProduitComponent } from './modules/produits/create-produit/create-produit.component';
-import { GestionProduitsComponent } from './modules/produits/gestion-produits/gestion-produits.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { EditFacturesComponent } from './modules/factures/edit-factures/edit-factures.component';
-import { ApercuFacturesComponent } from './modules/factures/apercu-factures/apercu-factures.component';
-import { NomClientComponent } from './modules/clients/nom-client/nom-client.component';
-import { ListeProduitsComponent } from './modules/produits/liste-produits/liste-produits.component';
-import { GestionProduitsInCreationFactureComponent } from './modules/produits/gestion-produits-in-creation-facture/gestion-produits-in-creation-facture.component';
-import { GestionProduitsInEditFactureComponent } from './modules/produits/gestion-produits-in-edit-facture/gestion-produits-in-edit-facture.component';
 import { AuthGuard } from './auth/auth.guard';
+import { DashboardComponent } from './modules/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
-    RegisterComponent, LoginComponent,
-    CreateClientComponent, EditClientComponent,
-    EditUserComponent, ListeFacturesComponent, CreateFactureComponent,
-    ListeClientsComponent, SelectClientComponent,
-    DetailClientComponent, EditProduitComponent, CreateProduitComponent
-,GestionProduitsComponent, EditFacturesComponent, ApercuFacturesComponent, 
-NomClientComponent, ListeProduitsComponent , GestionProduitsInCreationFactureComponent,
-GestionProduitsInEditFactureComponent ],
+
+
+    ListeFacturesComponent, 
+    ListeClientsComponent,
+  DashboardComponent
+
+  ],
   imports: [
     BrowserModule, //.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule, BrowserAnimationsModule ,
@@ -54,24 +34,29 @@ GestionProduitsInEditFactureComponent ],
    CommonModule, /* necessaire for ngModel */
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent },
-      { path: 'new-client', component: CreateClientComponent ,
-            canActivate: [AuthGuard],},
-      { path: 'edit-client/:id', component: EditClientComponent,
-            canActivate: [AuthGuard], },
-      { path: 'edit-user/:id', component: EditUserComponent ,
-            canActivate: [AuthGuard],},
-      { path: 'new-facture', component: CreateFactureComponent ,
-            canActivate: [AuthGuard],},
-      { path: 'edit-facture/:id', component: EditFacturesComponent ,
-            canActivate: [AuthGuard],
+
+      {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
       },
-      { path: 'facture/:id', component: ApercuFacturesComponent, 
-            canActivate: [AuthGuard],
-       },
+
+      {
+        path: 'client',
+        loadChildren: () => import('./modules/clients/client.module').then(m => m.ClientModule)
+      },
+
+      {
+        path: 'user',
+        loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule)
+      },
+      {
+        path: '',
+        loadChildren: () => import('./modules/factures/facture.module').then(m => m.FactureModule)
+      },
+
     ])
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
