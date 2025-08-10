5d4c1b9f9e97ce1dd27a0a10695e7bc4390d6b75
safiylm <121391404+safiylm@users.noreply.github.com>
2025-08-10 20:51:49 +0200

    page home affiche le dashboard uniquement si user est connecte

--- ClientApp/src/app/modules/home/home.component.ts
+++ ClientApp/src/app/modules/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,16 +7,5 @@ import { Router } from '@angular/router';
 
 })
 export class HomeComponent {
- nbClients = localStorage.getItem("nbClients")
- factureEnAttente = localStorage.getItem("factureEnAttente")
-  total = localStorage.getItem("totalFacture")
-
-  constructor( private route: Router) { }
-  logout() {
-    localStorage.setItem("isLoggedIn", "false")
-    localStorage.setItem('userId', '');
-    localStorage.removeItem('userId')
-    localStorage.removeItem('token');
-    this.route.navigate(['/login']);
-  }
+  connecte = localStorage.getItem("isLoggedIn")
 }
