a8f1a4c582c514434a18dae95f46d03acbf81c24
safiylm <121391404+safiylm@users.noreply.github.com>
2025-08-13 13:35:55 +0200

    TEST of edit product

--- ClientApp/src/app/modules/produits/edit-produit/edit-produit.component.spec.ts
+++ ClientApp/src/app/modules/produits/edit-produit/edit-produit.component.spec.ts
@@ -1,21 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { EditProduitComponent } from './edit-produit.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Produit } from 'src/app/models/produit.model';
 
 describe('EditProduitComponent', () => {
   let component: EditProduitComponent;
   let fixture: ComponentFixture<EditProduitComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach( () => {
+     TestBed.configureTestingModule({
       declarations: [ EditProduitComponent ], 
       imports:[HttpClientTestingModule]
     })
     .compileComponents();
-  });
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(EditProduitComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -24,4 +22,29 @@ describe('EditProduitComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+   it('should EDIT PRODUCT && SENT TO PARENT COMPONENT', () => {
+    const now = new Date();
+       
+    //on edit 
+    component.produit= new Produit(1, 2, 'PANTALON H&M',
+          39.0, 0.95, now, 2);
+
+
+    //on envoie les modif au parents
+    let emittedValue: any | undefined;
+    component.editProduitEvent.subscribe(value => emittedValue = value);
+    component.edit();
+    expect(emittedValue).not.toBeNull()
+
+
+    expect(emittedValue).toBeTruthy();
+    expect(emittedValue.quantite).toBe(2);
+    expect(emittedValue.designation).toBe('PANTALON H&M');
+    expect(emittedValue.prixUnitaireHT).toBe(39.0);
+    expect(emittedValue.tva).toBe(0.95);
+    expect(emittedValue.factureId).toBe(2);
+    expect(new Date(emittedValue.createdAt).getTime()).toBe(now.getTime());
+
+  })
 });
