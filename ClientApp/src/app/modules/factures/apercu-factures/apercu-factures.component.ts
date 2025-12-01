07fe61e6837ad5f289702578da9b9c03c6a792a8
safiylm <121391404+safiylm@users.noreply.github.com>
2025-12-01 08:48:07 +0100

    convert pdf + delete facture

--- ClientApp/src/app/modules/factures/apercu-factures/apercu-factures.component.ts
+++ ClientApp/src/app/modules/factures/apercu-factures/apercu-factures.component.ts
@@ -2,26 +2,56 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FactureService } from '../../../core/facture-service';
 import { Facture } from '../../../models/facture.model';
-
+import * as jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-apercu-factures',
   templateUrl: './apercu-factures.component.html',
   styleUrls: ['./apercu-factures.component.css']
 })
+
 export class ApercuFacturesComponent implements OnInit {
 
   constructor(private route: ActivatedRoute, private factureService: FactureService) { }
 
   facture!: Facture;
 
-  id !: number ;
+  id !: number;
+
+  download() {
+
+    const data = document.getElementById('facture');
+    html2canvas(data!).then(canvas => {
+      const imgWidth = 208;
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+      const contentDataURL = canvas.toDataURL('image/png');
+      const pdf = new jsPDF.jsPDF('p', 'mm', 'a4'); // A4 size page of PDF
+      const position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight);
+      pdf.save('facture' + this.facture.id + '.pdf'); // Save the generated PDF
+    });
+  }
 
   ngOnInit(): void {
-    this.id = Number( this.route.snapshot.paramMap.get('id')!);
+    this.id = Number(this.route.snapshot.paramMap.get('id')!);
 
-    this.factureService.getFactureById( this.id ).subscribe(
+    this.factureService.getFactureById(this.id).subscribe(
       (data) => {
-       this.facture = data
+        this.facture = data
+        // if (data == null) {
+        //   location.href = '/'
+        // }
+      })
+
+  }
+
+  delete() {
+    this.factureService.delete(this.id).subscribe(
+      (data) => {
+        if (data) {
+          if (data == '{"message":"Facture is deleted with success."}')
+            location.href = '/'
+        }
       })
   }
 
